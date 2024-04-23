8908bcf054b90fc288ec540890c65ddf4f82ea43
hjcs123 <u22230988@utp.edu.pe>
2024-04-23 16:54:28 -0500

    Crear Categoría

---
+++ src/main/java/org/example/Beans/Categoria.java
@@ -0,0 +1,36 @@
+package org.example.Beans;
+
+public class Categoria {
+
+    private int id;
+    private String name;
+
+    public Categoria() {
+    }
+
+    public Categoria(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
